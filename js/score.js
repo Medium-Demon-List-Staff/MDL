a606a1bab0bb727761836da5d7331679e9e702cc
Angeb-hub <angmen0214@gmail.com>
2025-08-29 00:32:03 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-0.755895*Math.pow(rank-1, 0.5) + 40) *
+    let score = (-0.755895*Math.pow(rank-1, 0.5) + 50) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
