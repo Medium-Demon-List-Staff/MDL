57cacd822d924fe60aa417e550631c927fa1dc97
Chahnkeh <huntermound@protonmail.com>
2025-10-26 14:41:01 -0700

    Update content.js
    Added pack integration and distinguished verifiers.

--- js/content.js
+++ js/content.js
@@ -1,3 +1,4 @@
+import { fetchPacks, fetchPackLevels } from "../content.js";
 import { round, score } from './score.js';
 
 /**
@@ -7,6 +8,7 @@ const dir = '/data';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
+    const packResult = await fetch(`${dir}/_packlist.json`);
     try {
         const list = await listResult.json();
         return await Promise.all(
@@ -14,9 +16,15 @@ export async function fetchList() {
                 const levelResult = await fetch(`${dir}/${path}.json`);
                 try {
                     const level = await levelResult.json();
+                    level.verifier = nameMap[level.verifier] || level.verifier;
+                    level.author = nameMap[level.author] || level.author;
+                    level.creators = level.creators.map((creator) => nameMap[creator] || creator);
+                    let packs = packsList.filter((x) =>
+                        x.levels.includes(path)
                     return [
                         {
                             ...level,
+                            packs,
                             path,
                             records: level.records.sort(
                                 (a, b) => b.percent - a.percent,
@@ -48,7 +56,7 @@ export async function fetchEditors() {
 
 export async function fetchLeaderboard() {
     const list = await fetchList();
-
+    const packResult = await (await fetch(`${dir}/_packlist.json`)).json();
     const scoreMap = {};
     const errs = [];
     list.forEach(([level, err], rank) => {
@@ -56,6 +64,24 @@ export async function fetchLeaderboard() {
             errs.push(err);
             return;
         }
+
+        // Verification
+        const verifier = Object.keys(scoreMap).find(
+            (u) => u === level.verifier,
+        ) || level.verifier;
+        scoreMap[verifier] ??= {
+            verified: [],
+            completed: [],
+            progressed: [],
+            packs: [],
+        };
+        const { verified } = scoreMap[verifier];
+        verified.push({
+            rank: rank + 1,
+            level: level.name,
+            score: score(rank + 1, 100, level.percentToQualify),
+            link: level.verification,
+        });
 
         // Records
         level.records.forEach((record) => {
@@ -66,6 +92,7 @@ export async function fetchLeaderboard() {
                 verified: [],
                 completed: [],
                 progressed: [],
+                packs: [],
             };
             const { completed, progressed } = scoreMap[user];
             if (record.percent === 100) {
@@ -87,7 +114,18 @@ export async function fetchLeaderboard() {
             });
         });
     });
-
+    for (let user of Object.entries(scoreMap)) {
+        let levels = [...user[1]["verified"], ...user[1]["completed"]].map(
+            (x) => x["path"]
+        );
+        for (let pack of packResult) {
+            if (pack.levels.every((e1) => levels.includes(e1))) {
+                user[1]["packs"].push(pack);
+            }
+        }
+        
+    }
+    
     // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
         const { verified, completed, progressed } = scores;
@@ -104,4 +142,52 @@ export async function fetchLeaderboard() {
 
     // Sort by total score
     return [res.sort((a, b) => b.total - a.total), errs];
+}
+export async function fetchPacks() {
+    try {
+        const packResult = await fetch(`${dir}/_packlist.json`);
+        const packsList = await packResult.json();
+        return packsList;
+    } catch {
+        return null;
+    }
+}
+
+export async function fetchPackLevels(packname) {
+    const packResult = await fetch(`${dir}/_packlist.json`);
+    const packsList = await packResult.json();
+    const selectedPack = await packsList.find((pack) => pack.name == packname);
+    const nameMap = await fetchNameMap();
+    try {
+        return await Promise.all(
+            selectedPack.levels.map(async (path, rank) => {
+                const levelResult = await fetch(`${dir}/${path}.json`);
+                try {
+                    const level = await levelResult.json();
+                    level.verifier = nameMap[level.verifier] || level.verifier;
+                    level.author = nameMap[level.author] || level.author;
+                    level.creators = level.creators.map((creator) => nameMap[creator] || creator);
+
+
+                    return [
+                        {
+                            level,
+                            path,
+                            records: level.records.map((record) => {
+                                record.user = nameMap[record.user] || record.user;
+                                return record;
+                            }),
+                        },
+                        null,
+                    ];
+                } catch {
+                    console.error(`Failed to load level #${rank + 1} ${path}.`);
+                    return [null, path];
+                }
+            })
+        );
+    } catch (e) {
+        console.error(`Failed to load packs.`, e);
+        return null;
+    }
 }
