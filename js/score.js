763372b99318400ec737ac5d73f73a2855f34a67
Chahnkeh <huntermound@protonmail.com>
2025-10-27 08:27:31 -0700

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-1.641476*Math.pow(rank-1, 0.5) + 40) *
+    let score = (-1.641477*Math.pow(rank-1, 0.5) + 40) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
