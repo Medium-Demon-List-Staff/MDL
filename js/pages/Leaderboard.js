ccab1b7a3e1dc88f7d74742c817a7111378221a9
Chahnkeh <huntermound@protonmail.com>
2026-01-02 11:40:28 -0800

    Make it pretty

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -44,22 +44,27 @@ export default {
                 <div class="player-container surface">
                     <div class="player">
                         <h1 v-if="entry">#{{ selected + 1 }} {{ entry.user }}</h1>
-                        <h3>{{ entry.total }}</h3>
+                        <h3 v-if="entry">{{ entry.total }}</h3>
 
                 <!-- PACKS -->
                         <h2 v-if="entry.packs?.length">Packs</h2>
-                        <div v-if="entry.packs?.length" style="display:flex;gap:6px;">
+                        <div v-if="entry.packs?.length" style="display:flex;flex-wrap:wrap;gap:10px;margin-top:8px;">
                     <div v-for="pack in entry.packs"
-            class="tag"
-            :style="{ background: pack.colour, color: getFontColour(pack.colour) }">
-            {{ pack.name }}
-        </div>
+                        class="tag"
+                        :style="{
+                            background: pack.colour,
+                            color: getFontColour(pack.colour),
+                            padding: '8px 12px',
+                            borderRadius: '14px',
+                            lineHeight: '1.1'
+                        }">
+                        {{ pack.name }}
+                    </div>
     </div>
 
     <h2 v-if="entry.verified.length > 0">
         Verified ({{ entry.verified.length}})
     </h2>
-    ...
 
                         <h2 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h2>
                         <table v-if="entry.verified.length > 0" class="table">
